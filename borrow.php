d950e36f1425a656a7f5471aac6b18dfc714df42
isapas <isabelle.passeniers@gmail.com>
2018-11-21 09:45:21 +0100

    login form finished

---
+++ borrow.php
@@ -0,0 +1,48 @@
+<?php
+include "template/header.php";
+?>
+<main>
+  <div class="container">
+    <section class="row">
+      <h1>Emprunter du matériel</h1>
+      <select>
+        <option value="Trier">Trier</option>
+        <option value="disponibilité">Disponibilité</option>
+        <option value="Asc.">Asc.</option>
+        <option value="Desc.">Desc.</option>
+      </select>
+    </section>
+    </div>
+    <div class="container">
+      <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+    </div>
+</main>
