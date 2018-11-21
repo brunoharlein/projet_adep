b0da6048c51b94d4f675c86c28f954b2c62238b0
isapas <isabelle.passeniers@gmail.com>
2018-11-21 14:03:24 +0100

    intégration page emprunts responsive

--- borrow.php
+++ borrow.php
@@ -3,10 +3,10 @@ include "template/header.php";
 ?>
 <main>
   <div class="container">
-    <section class="row">
-      <h1>Emprunter du matériel</h1>
-      <select>
-        <option value="Trier">Trier</option>
+    <section class="d-flex flex-row justify-content-between">
+      <h1 class="col-4 mt-0">Emprunter du matériel</h1>
+      <select class=" btn-primary browser-default custom-select col-4 mt- ">
+          <option selected>Trier par:</option>
         <option value="disponibilité">Disponibilité</option>
         <option value="Asc.">Asc.</option>
         <option value="Desc.">Desc.</option>
@@ -14,35 +14,28 @@ include "template/header.php";
     </section>
     </div>
     <div class="container">
-      <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+      <table class="table table-hover">
+        <thead class="thead-light">
+          <tr>
+            <th scope="col-2" >Matériel</th>
+            <th scope="col-2" class="d-none d-md-table-cell">Descrition</th>
+            <th scope="col-2" class="d-none d-md-table-cell">Etat</th>
+            <th scope="col-2" class="d-none d-md-table-cell">Accessibilité</th>
+            <th scope="col-2" >Emprunter</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td scope="row"></td>
+            <td class="d-none d-md-table-cell"></td>
+            <td class="d-none d-md-table-cell"></td>
+            <td class="d-none d-md-table-cell"></td>
+            <td class="btn btn-primary btn-xs">Emprunter</td>
+          </tr>
+
+        </tbody>
+      </table>
     </div>
 </main>
+<?php
+include "template/footer.php";
