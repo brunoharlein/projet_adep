75a9961dc517d13f2c6b52435946d6eb69e041ab
Benjamin <paspense@gmail.com>
2018-11-22 09:32:38 +0100

    update db

--- modele/db.php
+++ modele/db.php
@@ -2,7 +2,7 @@
 //We try to connect to the db with our id
 //If it works we create a variable db which stocks the connexion
 try {
-  $db = new PDO("mysql:host=localhost;dbname=EmpruntsAdep", "phpmyadmin", "asma2012");
+  $db = new PDO("mysql:host=localhost;dbname=EmpruntsAdep", "ben", "test");
 }
 //Sinon on récupère une erreur
 catch (Exception $e) {
