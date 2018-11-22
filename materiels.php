bcf570750222aa65cf8971efda5e0e91d862b47a
Fatma <fatma.elammari@gmail.com>
2018-11-22 11:10:21 +0100

    responsive table Materiels

--- materiels.php
+++ materiels.php
@@ -11,44 +11,43 @@ include "template/header.php";
             <div class="">
                <a href="materielsAdmin.php?action=add" class="btn btn-primary">Ajouter un matériel</a>
             </div>
-          </div>
+      </div>
+      <div class="table-responsive">
+        <table class="table table-hover">
+                <thead>
+                  <tr class="text-center">
+                    <th class="col-3 text-left">Nom</th>
+                    <th class="col-2 d-none d-md-table-cell">N° de série</th>
+                    <th class="col-3 d-none d-md-table-cell">Description</th>
+                    <th class="col-1 d-none d-lg-table-cell">Etat</th>
+                    <th class="col-1 d-none d-lg-table-cell">Accessibilité</th>
+                    <th class="col-1">Modifier</th>
+                    <th class="col-1">Supprimer</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php
+                    // On récupère tout le contenu de la table materiel
+                    $result = getMateriels($db);
+                    // On affiche chaque entrée une à une
+                    foreach ($result as $key => $value) {
+                  ?>
+                  <tr class="text-center">
+                    <td class="text-left"><?php echo $value["nom"]; ?></td>
+                    <td class="d-none d-md-table-cell"><?php echo $value["num_serie"]; ?></td>
+                    <td class="d-none d-md-table-cell"><?php echo $value["description"]; ?></td>
+                    <td class="d-none d-lg-table-cell"><?php echo ($value["etat"] == 1)?"En stock":"Indisponible"; ?></td>
+                    <td class="d-none d-lg-table-cell"><?php echo ($value["acces"] == 1)?"Libre":"Restreint"; ?></td>
+                    <td><a href="materielsAdmin.php?action=edit&id=<?php echo $value["id"]; ?>"><i class="fas fa-edit fa-2x"></i></a></td>
+                    <td><a href="materielsAdmin.php?action=delete&id=<?php echo $value["id"]; ?>"><i class="fas fa-times fa-2x"></i></a></td>
+                  </tr>
+                  <?php
+                    }
+                  ?>
+              </tbody>
+            </table>
+      </div>
 
-      <table class="table table-hover">
-              <thead>
-                <tr class="text-center">
-                  <th class="text-left w-25">Nom</th>
-                  <th class="d-none d-md-table-cell">N° de série</th>
-                  <th class="d-none d-md-table-cell">Description</th>
-                  <th class="d-none d-lg-table-cell">Etat</th>
-                  <th class="d-none d-lg-table-cell">Accessibilité</th>
-                  <th>Modifier</th>
-                  <th>Supprimer</th>
-                </tr>
-              </thead>
-              <tbody>
-                <?php
-                  // On récupère tout le contenu de la table materiel
-                  $result = getMateriels($db);
-                  // On affiche chaque entrée une à une
-                  foreach ($result as $key => $value) {
-
-                ?>
-                <tr class="text-center">
-                  <td class="text-left"><?php echo $value["nom"]; ?></td>
-                  <td class="d-none d-md-table-cell"><?php echo $value["num_serie"]; ?></td>
-                  <td class="d-none d-md-table-cell"><?php echo $value["description"]; ?></td>
-                  <td class="d-none d-lg-table-cell"><?php echo ($value["etat"] == 1)?"En stock":"Indisponible"; ?></td>
-                  <td class="d-none d-lg-table-cell"><?php echo ($value["acces"] == 1)?"Libre":"Restreint"; ?></td>
-                  <td><a href="materielsAdmin.php?action=edit&id=<?php echo $value["id"]; ?>"><i class="fas fa-edit fa-2x"></i></a></td>
-                  <td><a href="materielsAdmin.php?action=delete&id=<?php echo $value["id"]; ?>"><i class="fas fa-times fa-2x"></i></a></td>
-                </tr>
-                <?php
-                  }
-
-                ?>
-            </tbody>
-
-          </table>
   </div>
 </section>
 </main>
