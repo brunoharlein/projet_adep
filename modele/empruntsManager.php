80008b0dc8c8f502f5a93f3dccc48bb86b949f57
isapas <isabelle.passeniers@gmail.com>
2018-11-23 09:54:37 +0100

    function updateMateriel

--- modele/empruntsManager.php
+++ modele/empruntsManager.php
@@ -1,9 +1,5 @@
 <?php
-  /*function nouvelEmprunt($db, $id_materiel, $id_emprunteur){
-    $id_materiel = $_GET['materiel']['id'];
-    $id_emprunteur = $_SESSION['id'];
-    $requete = $db ->prepare('INSERT INTO emprunt(id, id_emprunteur, id_materiel, date_emprunt, date_retour) VALUES (id)')
-}*/
+
     //fonction qui update l'état du matériel en fonction de son id quand l'utilisateur  clique sur emprunter page emprunts.php
       function updateEtatMateriel ($db, $id_materiel, $etat) {
 
@@ -11,4 +7,5 @@
         $result = $requete->execute(array('id' => $id_materiel, 'etat' => $etat));
         return $result;
       }
+       function nouvelEmprunt($db, )
      ?>
--- service/empruntsTraitement.php
+++ service/empruntsTraitement.php
@@ -2,8 +2,16 @@
   require "../modele/db.php";// pour connecter a la db
   require "../modele/empruntsManager.php"; //pour les fonctions qui servent à l'emprunt
   $id_materiel = $_GET['id'];
-  $etat =$_GET['etat'];
-  updateEtatMateriel ($db, $id_materiel, $etat) ;
+  $id_emprunteur = $_SESSION['id'];
+  $etat = $_GET['etat'];
+  session_start;
+
+  if (isset($_GET['id']) && (!empty($_GET['id'])) {
+    if (isset($_GET['etat']) && (!empty($_GET[etat]))){
+      updateEtatMateriel ($db, $id_materiel, $etat);
+    }
+  }
+  
 
 
 ?>
