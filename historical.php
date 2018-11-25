f7bb086fe54d2cb6365ca48b9b4dd4a199f42261
bruno.h <bruno@harlein.com>
2018-11-25 23:30:07 +0100

    update fix bug and create select choice historical.php

--- historical.php
+++ historical.php
@@ -8,6 +8,25 @@ $historicals = getHistorical($db);
  <div class="row mt-5">
     <section class="col-md-6 mx-auto mt-4">
       <h2 class="historicalH2">Gestion des emprunts</h2>
+      <form action="historical.php<?php echo (isset($_POST['choix']))?'?tri='.$_POST['choix']:''; ?>" method="post" name="tri">
+      <div class="form-row align-items-center">
+      <div class="col-auto my-1">
+      <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Preference</label>
+      <select name="choix" class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+      <option selected>Options</option>
+      <option value="1">noms de familles de A à Z</option>
+      <option value="2">noms de familles Z à A</option>
+      <option value="3">noms des appareils</option>
+      <option value="4">date emprunt</option>
+    </select>
+  </div>
+  <div class="col-auto my-1">
+  </div>
+  <div class="col-auto my-1">
+    <button type="submit" class="btn btn-primary">OK</button>
+  </div>
+</div>
+</form>
       <div class="container-fluide">
         <div class="row">
           <table class="table table-hover fontTable">
