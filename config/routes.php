f1d576110ac50ebbf2e238581fcb3f785d310348
Bruno HARLEIN <bruno@harlein.com>
2018-12-18 16:06:52 +0100

    update

--- config/routes.php
+++ config/routes.php
@@ -14,14 +14,7 @@
 //]
 function getRoutes() {
   return [
-    "" => [
-      "index",
-      "login"
-    ],
-    "login" => [
-      "index",
-      "login"
-    ],
+
     ////////////////////////////////start roads for material section
 
     "materiels" => [
